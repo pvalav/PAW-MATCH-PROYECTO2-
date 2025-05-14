3c9460f5f596be3893ba9f9f382252dead3c871e
Patricia A <pvaleriavr@gmail.com>
2025-05-14 15:26:18 -0600

    Se modifico el archivo app.js, el inicio.ejs

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("inicio", { titulo: "inicio" });
+});
+
+app.listen(9999, () => {
+  console.log("Servidor corriendo en http://localhost:9999");
+});
