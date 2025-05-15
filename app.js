8edd13719f71cd1f0fa9d91c27240b1f3f1dad8b
Patricia A <pvaleriavr@gmail.com>
2025-05-14 18:05:01 -0600

    Se agrego el registro de usuarios

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("inicio", { titulo: "inicio" });
 });
+app.get("/registro1",(req,res)=>{
+    res.render("registro1",{titulo:"registro"});
+})
 
 app.listen(9999, () => {
   console.log("Servidor corriendo en http://localhost:9999");
