3b76d47f0c185f0eee6c84a09cbed8eebd476116
Patricia A <pvaleriavr@gmail.com>
2025-05-15 17:31:03 -0600

    Se modificaron ejs y se agregaron mas archivos

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const mysql=require("mysql2");
 const app = express();
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '', 
+  database: 'pawmatch'
+});
+db.connect(err => {
+  if (err) {
+    console.error('Error de conexión:', err);
+    return;
+  }
+  console.log('Conectado a MySQL');
+});
 
 app.use(express.static("public"));
 
@@ -8,12 +23,13 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("inicio", { titulo: "inicio" });
 });
-app.get("/registro1",(req,res)=>{
-    res.render("registro1",{titulo:"registro"});
-})
-app.get("/registro2",(req,res)=>{
-    res.render("registro2",{titulo:"registro2"});
+app.get("/ayuda",(req,res)=>{
+  res.render("ayuda",{titulo:"ayuda"});
 });
+app.get("/registro1",(req,res)=>{
+  res.render("registro1",{titulo:"registro"});
+});
+
 
 app.listen(9999, () => {
   console.log("Servidor corriendo en http://localhost:9999");
